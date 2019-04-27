a5e9a4566cdc5a254b0967183c2eebce6dbd89fa
Roman <roman.kocenko.2004@mail.ru>
2019-04-27 20:07:18 +0200

    App which worked without another fragments(not mainWindowFragment)

--- app/src/main/java/com/example/mac/suchik/UI/MainActivityUI.java
+++ app/src/main/java/com/example/mac/suchik/UI/MainActivityUI.java
@@ -22,9 +22,6 @@ public class MainActivityUI extends AppCompatActivity implements InternetDialogF
     public android.support.v7.app.ActionBar actionbar;
 
     public static final int MAIN_WINDOW_FRAGMENT = 1;
-    public static final int SCHEDULE_WINDOW_FRAGMENT = 2;
-    public static final int MY_LIST = 3;
-    public static final int SETTINGS = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,15 +56,6 @@ public class MainActivityUI extends AppCompatActivity implements InternetDialogF
             case R.id.main_window_btn:
                 openFragment(MAIN_WINDOW_FRAGMENT);
                 return true;
-            case R.id.schedule_btn:
-                openFragment(SCHEDULE_WINDOW_FRAGMENT);
-                return true;
-            case R.id.list_btn:
-                openFragment(MY_LIST);
-                return true;
-            case R.id.settings_btn:
-                openFragment(SETTINGS);
-                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -75,15 +63,6 @@ public class MainActivityUI extends AppCompatActivity implements InternetDialogF
     public void openFragment(int fragmentId) {
         Fragment fragment = null;
         switch (fragmentId) {
-            case SCHEDULE_WINDOW_FRAGMENT:
-                fragment = new Schedule();
-                break;
-            case MY_LIST:
-                fragment = new TimeTable();
-                break;
-            case SETTINGS:
-                fragment = new Settings();
-                break;
             case MAIN_WINDOW_FRAGMENT:
             default:
                 fragment = new MainWindowFragment();
